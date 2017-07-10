4dd7e63c6589bb9ba7abb9fa3d195610f592c281
SHAFI ul BARI <shafiul.shafi@gmail.com>
2017-07-10 13:57:41 +0600

    header text and dynamic link added

--- header.php
+++ header.php
@@ -10,47 +10,59 @@
  */
 
 ?>
-<!DOCTYPE HTML>
-<html <?php language_attributes(); ?>>
+	<!DOCTYPE HTML>
+	<html <?php language_attributes(); ?>>
+
 	<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php wp_head(); ?>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php wp_head(); ?>
 	</head>
+
 	<body <?php body_class();?> >
 
 		<!-- Header -->
-			<section id="header">
-					<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mysite' ); ?></a>
+		<section id="header">
+			<a class="skip-link screen-reader-text" href="#content">
+				<?php esc_html_e( 'Skip to content', 'mysite' ); ?>
+			</a>
 
-				<header>
-					<span class="image avatar">
-<?php 
-// get and set the custom logo image
-$custom_logo_id = get_theme_mod ('custom_logo');
-$custom_logo_src = wp_get_attachment_image_src ($custom_logo_id, 'full');
-?>
+			<header>
+				<span class="image avatar">
+					<?php 
+					// get and set the custom logo image
+					$custom_logo_id = get_theme_mod ('custom_logo');
+					$custom_logo_src = wp_get_attachment_image_src ($custom_logo_id, 'full');
+					?>
 
-						<img src="<?php echo $custom_logo_src[0]; ?>" alt="" /></span>
-					<h1 id="logo"><a href="#">Willis Corto</a></h1>
-					<p>I got reprogrammed by a rogue AI<br />
-					and now I'm totally cray</p>
-				</header>
-				<nav id="nav">
-					<ul>
-						<li><a href="#one" class="active">About</a></li>
-						<li><a href="#two">Things I Can Do</a></li>
-						<li><a href="#three">A Few Accomplishments</a></li>
-						<li><a href="#four">Contact</a></li>
-					</ul>
-				</nav>
-				<footer>
-					<ul class="icons">
-						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-						<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
-					</ul>
-				</footer>
-			</section>
+						<img src="<?php echo $custom_logo_src[0]; ?>" alt="" />
+				</span>
+				<!--front page title-->
+				
+				<?php if (is_front_page() && is_home() ) : ?>
+
+				<h1 id="logo">
+					<!--esc_url() sanitizes the url provided by home_url -->
+					<!--home_url() provides the root url of the site-->
+					<a href="<?php echo esc_url (home_url('/')); ?>" rel="home"><?php bloginfo ('name'); ?></a></h1>
+					<?php endif; ?>
+				<p>I got reprogrammed by a rogue AI<br /> and now I'm totally cray</p>
+			</header>
+			<nav id="nav">
+				<ul>
+					<li><a href="#one" class="active">About</a></li>
+					<li><a href="#two">Things I Can Do</a></li>
+					<li><a href="#three">A Few Accomplishments</a></li>
+					<li><a href="#four">Contact</a></li>
+				</ul>
+			</nav>
+			<footer>
+				<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+					<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+				</ul>
+			</footer>
+		</section>
