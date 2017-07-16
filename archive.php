953ebbccb30e5da38e7eeeb513304da4d2e7bbae
SHAFI ul BARI <shafiul.shafi@gmail.com>
2017-07-16 22:45:44 +0600

    archive.php file modified

--- archive.php
+++ archive.php
@@ -18,10 +18,13 @@ get_header(); ?>
 	<div id="main">
 		<?php if (have_posts() ) :?>
 		<header class="page-header">
-			<?php 
-				the_archive_title('<h1 class="page-title">', '</h1>');
-				the_archive_description( '<div class="archive-description">', '</div>' )				
-			?>
+ 			<div class="container">
+ 				<?php 
+				the_archive_title('<h2 class="page-title">', '</h2>');
+				the_archive_description( '<div class="archive-description">', '</div>' );		
+				?>
+ 			</div>
+ 			<!-- .container -->
 		</header>
 		<?php 
 							/* start the loop */
