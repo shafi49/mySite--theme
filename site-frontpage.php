cc0448d6702926b395aa2d385dcfe49c3a6c18f2
SHAFI ul BARI <shafiul.shafi@gmail.com>
2017-08-22 22:35:05 +0600

    contact form 7 added

--- site-frontpage.php
+++ site-frontpage.php
@@ -22,11 +22,11 @@ get_header();
 						<section id="one">
 							<div class="container">
 								<header class="major">
-									<h2>Read Only</h2>
-									<p>Just an incredibly simple responsive site<br />
+									<h2>Welcome!</h2>
+									<p>This is web developer Shafi's personal internet space<br />
 									</p>
 								</header>
-								<p>Faucibus sed lobortis aliquam lorem blandit. Lorem eu nunc metus col. Commodo id in arcu ante lorem ipsum sed accumsan erat praesent faucibus commodo ac mi lacus. Adipiscing mi ac commodo. Vis aliquet tortor ultricies non ante erat nunc integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum.</p>
+								<p>I ❤ surfing and desiging websites</p>
 							</div>
 						</section>
 
@@ -34,14 +34,12 @@ get_header();
 						<section id="two">
 							<div class="container">
 								<h3>Things I Can Do</h3>
-								<p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer lorem ipsum dolor sit amet.</p>
 								<ul class="feature-icons">
 									<li class="fa-code">Write all the code</li>
 									<li class="fa-cubes">Stack small boxes</li>
 									<li class="fa-book">Read books and stuff</li>
 									<li class="fa-coffee">Drink much coffee</li>
-									<li class="fa-bolt">Lightning bolt</li>
-									<li class="fa-users">Shadow clone technique</li>
+									<li class="fa-bolt">Code Lightning bolt websites</li>
 								</ul>
 							</div>
 						</section>
@@ -49,28 +47,31 @@ get_header();
 					<!-- Three -->
 						<section id="three">
 							<div class="container">
-								<h3>A Few Accomplishments</h3>
-								<p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer. Integer eu ante ornare amet commetus.</p>
+								<h3>My skills</h3>
+								<p>I have pretty solid understanding about these technologies</p>
 								<div class="features">
 									<article>
-										<a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
+										<a href="#" class="image"><i class="fa fa-html5" aria-hidden="true"></i>
+</a>
 										<div class="inner">
-											<h4>Possibly broke spacetime</h4>
-											<p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+											<h4>HTML</h4>
+											<p>The markup language that every web browser understands. </p>
 										</div>
 									</article>
 									<article>
-										<a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
+										<a href="#" class="image"><i class="fa fa-css3" aria-hidden="true"></i>
+</a>
 										<div class="inner">
-											<h4>Terraformed a small moon</h4>
-											<p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+											<h4>CSS</h4>
+											<p>The beauty rules of web browsers!</p>
 										</div>
 									</article>
 									<article>
-										<a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
+										<a href="#" class="image"><i class="fa fa-code" aria-hidden="true"></i>
+</a>
 										<div class="inner">
-											<h4>Snapped dark matter in the wild</h4>
-											<p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+											<h4>JavaScript</h4>
+											<p>The logic behind all interactions happening inside browsers.</p>
 										</div>
 									</article>
 								</div>
@@ -82,26 +83,8 @@ get_header();
 							<div class="container">
 								<h3>Contact Me</h3>
 								<p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer. Integer eu ante ornare amet commetus.</p>
-								<form method="post" action="#">
-									<div class="row uniform">
-										<div class="6u 12u(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-										<div class="6u 12u(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-									</div>
-									<div class="row uniform">
-										<div class="12u"><input type="text" name="subject" id="subject" placeholder="Subject" /></div>
-									</div>
-									<div class="row uniform">
-										<div class="12u"><textarea name="message" id="message" placeholder="Message" rows="6"></textarea></div>
-									</div>
-									<div class="row uniform">
-										<div class="12u">
-											<ul class="actions">
-												<li><input type="submit" class="special" value="Send Message" /></li>
-												<li><input type="reset" value="Reset Form" /></li>
-											</ul>
-										</div>
-									</div>
-								</form>
+								<?php echo do_shortcode( '[contact-form-7 id="1720" title="Contact form 1"]' )?>
+								
 							</div>
 						</section>
 
